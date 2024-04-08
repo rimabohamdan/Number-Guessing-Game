8bf2ab3cd77a9eebb45be5e1caeb9acee32cb56e
Rima Bohamdan <151973213+rimabohamdan@users.noreply.github.com>
2024-04-08 21:22:08 +0300

    Create number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,7 @@
+guessed = int(input("please guess a number:\n"))
+correct_number = 7
+if guessed == correct_number:
+    print("Good Guess")
+else:
+    print(f"You guessed {guessed} ,but the correct number is {correct_number}")
+  
